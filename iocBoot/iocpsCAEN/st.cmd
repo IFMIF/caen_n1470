3b4050c48ef8e3036ddde8f898343d8d13680dd0
Florian Benedetti <florian.benedetti@cea.fr>
2024-10-03 03:41:35 +0200

    Initial commit

---
+++ iocBoot/iocpsCAEN/st.cmd
@@ -0,0 +1,25 @@
+#!../../bin/linux-x86_64/DG645
+
+#- You may have to change DG645 to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TOP)/db"
+epicsEnvSet "IP" "192.168.1.5"
+epicsEnvSet "IPport" "4001"
+
+# Macro prefix
+epicsEnvSet "PORT" "N1470"
+
+## Register all support components
+dbLoadDatabase "dbd/psCAEN.dbd"
+psCAEN_registerRecordDeviceDriver pdbbase
+
+drvAsynIPPortConfigure($(PORT), "$(IP):$(IPport)",0,0,0) 
+dbLoadTemplate("db/psCAEN.substitutions")
+
+epicsThreadSleep(2)
+iocInit
+
